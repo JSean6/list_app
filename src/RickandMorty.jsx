bc8f51d136bbc0f6c29560c61ad360ecec8b9f2a
JSEAN6 <seannjoroge0@gmail.com>
2024-04-05 13:19:03 +0300

    add changes

---
+++ src/RickandMorty.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+const RickAndMorty = ()=>{
+    
+    const [characters, setCharacters] = useState([])
+    
+    
+    const fetchCharacters = async ()=>{
+    const response = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await response.json()
+
+    setCharacters(data.results)
+    }
+    
+    
+    useEffect (()=>{
+    fetchCharacters()
+    
+    }, [])
+    
+    return (
+    
+    <div>
+   
+    {characters.map((character)=>{
+    <div key={character.id}>
+    <div>{character.name}</div>
+    <img src={character.image} alt={character.name}/>
+    </div>
+    })}
+    </div>
+    )
+    }
+    
+    export default RickAndMorty;
